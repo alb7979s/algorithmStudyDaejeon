a6258bec64eebf70613d660f7d960de753662a1e
chelseashin <chaewonshin95@gmail.com>
2020-08-27 18:28:41 +0900

    200827 | Algorithm PS continue

--- chelseashin/모의SW역량테스트/5656_벽돌깨기.py
+++ chelseashin/모의SW역량테스트/5656_벽돌깨기.py
@@ -1,69 +1,92 @@
 import sys
 sys.stdin = open('5656_input.txt')
 
+# 나의 풀이(Optimization)
+
+# 현재 depth에서 무슨 작업을 하고 있는지 잊지 않고
+# depth 계속 잘 따져서 필요한 부분에 써주기
+
+from collections import deque
+
 dr = (-1, 1, 0, 0)
 dc = (0, 0, -1, 1)
 
-def down(depth):
-    remain = []
+# 벽돌 밑으로 내리기
+def gravity(depth):
     for c in range(W):
+        remain = []
         for r in range(H):
             if raw_data[depth][r][c]:
                 remain.append(raw_data[depth][r][c])
-        remain = [0] * (H - len(remain)) + remain
-        for r in range(H-1, -1, -1):
-            raw_data[depth][r][c] = remain.pop()
+                raw_data[depth][r][c] = 0
+        # R = len(remain)
+        for r in range(len(remain)):
+            raw_data[depth][H-r-1][c] = remain.pop()
 
-# 벽돌 깨기
-def boom(depth, sr, sc):
-    boom_count = 0
+
+# bfs로 터트려야 할 좌표들 넣어주기
+def bfs(sr, sc, depth):
     # 현재 depth에 작업하기 전에 전 depth의 결과를 복사해온다.
-    raw_data[depth+1] = [R[:] for R in raw_data[depth]]
-    stack = [(sr, sc)]
-    while stack:
-        r, c = stack.pop()
+    raw_data[depth + 1] = [R[:] for R in raw_data[depth]]
+
+    cnt = 0
+    Q = deque([(sr, sc)])
+    while Q:
+        r, c = Q.popleft()
         power = raw_data[depth+1][r][c]
         if power == 0:
             continue
         raw_data[depth+1][r][c] = 0
-        boom_count += 1
-
-        for i in range(4):
-            for j in range(1, power):
-                nr = r + dr[i] * j
-                nc = c + dc[i] * j
+        cnt += 1
+        
+        # 4방향 탐색하며 Q에 담기
+        for dir in range(4):
+            for p in range(1, power):
+                nr = r + dr[dir] * p
+                nc = c + dc[dir] * p
                 if not (0 <= nr < H and 0 <= nc < W):
                     continue
                 if not raw_data[depth+1][nr][nc]:
                     continue
-                stack.append((nr, nc))
-    down(depth+1)
-    return boom_count
+                Q.append((nr, nc))
 
-# 구슬 쏜 횟수, 깬 벽돌 수
-def dfs(depth, count):
-    global total
+    gravity(depth+1)
+    return cnt
+
+def dfs(depth, total):
+    global MIN
+    if depth == N:
+        MIN = min(MIN, brick-total)
+        return
     if total == brick:
+        MIN = 0
         return
-    # 구슬 N번 모두 쏘거나, 벽돌 다 깼을 경우
-    if depth == N or count == brick:
-        total = max(total, count)
-        return
-    for w in range(W):
-        for h in range(H):
-            if raw_data[depth][h][w]:
-                dfs(depth+1, count + boom(depth, h, w))
+    for c in range(W):
+        for r in range(H):
+            if raw_data[depth][r][c]:
+                count = bfs(r, c, depth)
+                dfs(depth+1, total+count)
                 break
 
 # main
 T = int(input())
 for tc in range(T):
     N, W, H = map(int, input().split())
-    raw_data = [[[0] * W for _ in range(H)] for _ in range(N+1)]
+    raw_data = [[] for _ in range(N+1)]     # 가진 구슬 갯수 + 1만큼
+    raw_data[0] = [list(map(int, input().split())) for _ in range(H)]
+    # print(raw_data)
     brick = 0
     for i in range(H):
-        raw_data[0][i] = list(map(int, input().split()))
-        brick += sum(1 for j in raw_data[0][i] if j)
-    total = 0
+        for j in range(W):
+            if raw_data[0][i][j]:
+                brick += 1
+
+    MIN = float('inf')
     dfs(0, 0)
-    print("#{} {}".format(tc+1, brick-total))
+    print('#{} {}'.format(tc+1, MIN))
+
+    # 1 12
+    # 2 27
+    # 3 4
+    # 4 8
+    # 5 0
