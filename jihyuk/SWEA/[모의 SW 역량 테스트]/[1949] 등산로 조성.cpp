2347b3f4ac1b0fd33b61bffeb3eaa5a04ab64bbe
jihyuk124 <jihyuk124@naver.com>
2020-08-31 11:43:00 +0900

    SWEA 모의 역량 테스트 2주차

---
+++ jihyuk/SWEA/[모의 SW 역량 테스트]/[1949] 등산로 조성.cpp
@@ -0,0 +1,110 @@
+﻿#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<memory.h>
+
+using namespace std;
+
+int T;
+int N, K;
+int maxPathLength;
+int terrain[8][8];
+int visited[8][8];
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
+int highest;
+
+void init()
+{
+	memset(visited, false, sizeof(visited));
+	maxPathLength = 0;
+	highest = 0;
+}
+
+void input()
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			scanf("%d", &terrain[i][j]);
+			if (highest < terrain[i][j])
+			{
+				highest = terrain[i][j];
+			}
+		}
+	}
+}
+
+bool isIn(int r, int c)
+{
+	return 0 <= r && r < N && 0 <= c && c < N;
+}
+
+void dfs(int r, int c, int len, int cutCnt)
+{
+	maxPathLength = max(maxPathLength, len);
+	visited[r][c] = true;
+
+    for (int idx = 0; idx < 4; idx++)
+	{
+        int dr = r + dx[idx];
+        int dc = c + dy[idx];
+
+		if (visited[dr][dc] || !isIn(dr, dc))
+		{
+			continue;
+		}
+
+        if (terrain[dr][dc] < terrain[r][c]) // 현재 지형보다 낮은 경우
+		{
+			dfs(dr, dc, len + 1, cutCnt);
+        }
+        else // 현재 지형보다 낮지 않은 경우(같거나 높은 경우)
+		{
+			if (cutCnt == 0 && terrain[dr][dc] - K < terrain[r][c])
+			{
+				int originHeight = terrain[dr][dc];
+
+				terrain[dr][dc] = terrain[r][c] - 1; // 현재 높이보다 한칸 낮은 높이로 설정한 후 진행
+				dfs(dr, dc, len + 1, 1);
+				terrain[dr][dc] = originHeight;
+			}
+        }
+    }
+	visited[r][c] = false;
+}
+
+void solve()
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (terrain[i][j] == highest && !visited[i][j])
+			{
+				visited[i][j] = true;
+				dfs(i, j, 1, 0);
+				visited[i][j] = false;
+			}
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	scanf("%d", &T);
+
+	for (int testCase = 1; testCase <= T; testCase++)
+	{
+		scanf("%d %d", &N, &K);
+
+		init();
+		input();
+		solve();
+
+		printf("#%d %d\n", testCase, maxPathLength);
+	}
+
+	return 0;
+}
---
+++ jihyuk/SWEA/[모의 SW 역량 테스트]/[2105] 디저트 카페.cpp
@@ -0,0 +1,90 @@
+﻿#include<cstdio>
+#include<memory.h>
+#include<algorithm>
+
+using namespace std;
+
+int T, N;
+int board[20][20];
+bool visited[101];
+int maxPathLength;
+int dx[] = { 1,-1,-1,1 };
+int dy[] = { 1,1,-1,-1 };
+int sr, sc;
+
+void init()
+{
+	memset(visited, 0, sizeof(bool) * 101);
+	maxPathLength = -1; // 경로가 존재하지 않는 경우
+}
+
+bool isIn(int r, int c)
+{
+	return 0 <= r && r < N && 0 <= c && c < N;
+}
+
+void dfs(int r, int c, int dir, int cnt)
+{
+	if (r == sr - 1 && c == sc + 1 && 4 <= cnt)
+	{
+		if (maxPathLength < cnt)
+		{
+			maxPathLength = cnt;
+		}
+		return;
+	}
+
+	for (int i = dir; i < 4; i++)
+	{
+		int dr = r + dx[i];
+		int dc = c + dy[i];
+
+		if (isIn(dr, dc) && false == visited[board[dr][dc]] && i - dir <= 1)
+		{
+			visited[board[dr][dc]] = true;
+			dfs(dr, dc, i, cnt + 1);
+			visited[board[dr][dc]] = false;
+		}
+	}
+}
+
+void solve()
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			sr = i;
+			sc = j;
+			visited[board[i][j]] = true;
+			dfs(i, j, 0, 1);
+			visited[board[i][j]] = false;
+		}
+	}
+}
+
+int main()
+{
+	scanf("%d", &T);
+
+	for (int testCase = 1; testCase <= T; testCase++)
+	{
+		scanf("%d", &N);
+
+		init();
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				scanf("%d", &board[i][j]);
+			}
+		}
+
+		solve();
+
+		printf("#%d %d\n", testCase, maxPathLength);
+	}
+
+	return 0;
+}
