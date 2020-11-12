059866f6466a82581033d4bdbd0a239664618e9d
jonghyeok <alb7979s@naver.com>
2020-11-12 11:16:59 +0900

    201112

--- jonghyeok/hw11_1/2840_행운의바퀴.py
+++ jonghyeok/hw11_1/2840_행운의바퀴.py
@@ -1,11 +1,22 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = [i for i in range(n+1)]
-    def find(self, u):
-        if self.parent[u] == u: return u
-        self.parent[u] = self.find(self.parent[u])
-        return self.parent[u]
-    def union(self, u, v):
-        u = self.find(u)
-        v = self.find(v)
-        self.parent[u] = v
+from sys import*
+from collections import*
+input=lambda:stdin.readline().strip()
+n, k = map(int,input().split())
+q=deque(['?'for _ in range(n)])
+#알파벳 한번씩만 사용가능...
+alp='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+visit={}
+for i in range(k):
+    inp=list(map(str,input().split()))
+    for j in range(int(inp[0])):
+        q.appendleft(q.pop())
+    if q[0]==inp[1] or (q[0]=='?' and inp[1] not in visit):
+        q[0] = inp[1]
+        visit[inp[1]]=1
+    else:
+        print('!')
+        exit()
+while q:
+    print(q.popleft(),end='')
+        
+    
